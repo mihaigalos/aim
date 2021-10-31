94dce43148ecee08637e85f0a145c78b6e2d3c94
Mihai Galos <mihaigalos@gmail.com>
2021-10-31 17:17:54 +0100

    Test incomplete resume via sha256

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,24 @@ pub async fn get(url: &str, path: &str) -> Result<(), String> {
 #[tokio::test]
 async fn get_works() {
     let out_file = "dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz";
-    get("https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", out_file).await;
+    
+    get("https://github.com/XAMPPRocky/tokei/releases/download/v12.0.4/tokei-x86_64-unknown-linux-gnu.tar.gz", out_file).await.unwrap();
+
     assert!(std::path::Path::new(out_file).exists());
+    std::fs::remove_file(out_file).unwrap();
+}
+
+#[tokio::test]
+async fn get_resume_works() {
+    let expected_hash = "16c241b0446b2b8ae8851f3facacd7604fe4193b2c0a545ae07652300f63a1e8";
+    let out_file = "test/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz";
+    
+    std::fs::copy("test/incomplete_dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", "test/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz").unwrap();
+    get("https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", out_file).await.unwrap();
+
+    let bytes = std::fs::read(out_file).unwrap();
+    let computed_hash = sha256::digest_bytes(&bytes);
+    
+    assert_eq!(computed_hash, expected_hash);
     std::fs::remove_file(out_file).unwrap();
 }
