4de8c36fcf497df9070a17f6209f20e9e152f622
Mihai Galos <mihaigalos@gmail.com>
2021-10-31 10:01:44 +0100

    Add write to file option

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ async fn main() {
         (version: crate_version!())
         (author: "Mihai Galos <mihaigalos at gmail dot com>")
         (about: "A simplified subset of curl written in Rust.")
+        (@arg FILE: -O --output +takes_value "write documents to FILE")
         (@arg URL: +required +takes_value "url to download")
         )
         .get_matches_safe().unwrap_or_else(|e| e.exit());
     
     let url = args.value_of("URL").unwrap();
+    let output_file = args.value_of("FILE").unwrap();
 
-    lib::download_file(url, "big_buck_bunny_480p_20mb.mp4").await.unwrap();
+    lib::download_file(url, output_file).await.unwrap();
 }
