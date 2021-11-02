628d68474769e736bfeb0736e5ec59ac094bff1c
Mihai Galos <mihaigalos@gmail.com>
2021-11-02 07:20:03 +0100

    Add ship symbol to progressbar message

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@ fn get_progress_bar(total_size: u64, url: &str) -> indicatif::ProgressBar {
     pb.set_style(ProgressStyle::default_bar()
         .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.white/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .progress_chars("█  "));
-    pb.set_message(&format!("Downloading {}", url));
+    pb.set_message(&format!("⛵ Downloading {}", url));
     pb
 }
 
@@ -72,7 +72,7 @@ pub async fn get(url: &str, path: &str) -> Result<(), String> {
         pb.set_position(new);
     }
 
-    pb.finish_with_message(&format!("Downloaded {} to {}", url, path));
+    pb.finish_with_message(&format!("⛵ Downloaded {} to {}", url, path));
     return Ok(());
 }
 
