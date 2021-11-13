aa5a288b2a8d741407024f7e9e2a72c646488bdc
Mihai Galos <mihaigalos@gmail.com>
2021-11-13 17:08:04 +0100

    Permit -O and -o output args

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ async fn main() {
     (author: "Mihai Galos <mihaigalos at gmail dot com>")
     (about: "⛵ Download/upload tool.")
     (@arg OUTPUT: -o --output +takes_value "Output to ship to. If not specified, writes to stdout (cannot resume).")
+    (@arg OUTPUT_ALT: -O +takes_value "Alternative to -o.")
     (@arg INPUT: +required +takes_value "Input to ship from.")
     (@arg silent: -s --silent "Silent or quiet mode. Don't show progress meter or error messages.")
     )
     .get_matches_safe()
     .unwrap_or_else(|e| e.exit());
     let url = args.value_of("INPUT").unwrap();
-    let output = args.value_of("OUTPUT").unwrap_or("");
+    let mut output = args.value_of("OUTPUT").unwrap_or("");
+    if output == "" {
+        output = args.value_of("OUTPUT_ALT").unwrap_or("");
+    }
+
     let silent = args.is_present("silent");
 
     ship::driver::Driver::drive(url, output, silent).await;
