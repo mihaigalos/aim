c5dbe045abed78694ca1a92dbdc4e3e310a3d445
Mihai Galos <mihaigalos@gmail.com>
2021-11-14 12:17:51 +0100

    Improve wording

--- src/driver.rs
+++ src/driver.rs
@@ -1,13 +1,13 @@
 pub struct Driver;
 
 impl Driver {
-    pub async fn drive(url: &str, output: &str, silent: bool) {
-        match &url[0..4] {
-            "ftp:" | "ftp." => crate::ftp::FTPHandler::get(url, output, silent).await,
-            "http" => crate::https::HTTPSHandler::get(url, output, silent)
+    pub async fn drive(input: &str, output: &str, silent: bool) {
+        match &input[0..4] {
+            "ftp:" | "ftp." => crate::ftp::FTPHandler::get(input, output, silent).await,
+            "http" => crate::https::HTTPSHandler::get(input, output, silent)
                 .await
                 .unwrap(),
-            _ => println!("Cannot exctract handler from URL: {} Exiting.", url),
+            _ => println!("Cannot extract handler from input: {} Exiting.", input),
         }
     }
 }
