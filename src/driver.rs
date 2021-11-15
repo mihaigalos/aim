95145603faa48175f75543730e165b68e6ad959d
Mihai Galos <mihaigalos@gmail.com>
2021-11-15 17:48:28 +0100

    Unify to GET verb

--- src/driver.rs
+++ src/driver.rs
@@ -1,12 +1,14 @@
 pub struct Driver;
 
+trait RESTVerbs {
+    fn get(url: &str, path: &str, silent: bool);
+}
+
 impl Driver {
     pub async fn drive(input: &str, output: &str, silent: bool) {
         match &input[0..4] {
             "ftp:" | "ftp." => crate::ftp::FTPHandler::get(input, output, silent).await,
-            "http" => crate::https::HTTPSHandler::get(input, output, silent)
-                .await
-                .unwrap(),
+            "http" => crate::https::HTTPSHandler::get(input, output, silent).await,
             _ => println!("Cannot extract handler from input: {} Exiting.", input),
         }
     }
--- src/https.rs
+++ src/https.rs
@@ -7,7 +7,7 @@ use crate::output::get_output;
 
 pub struct HTTPSHandler;
 impl HTTPSHandler {
-    pub async fn get(url: &str, path: &str, silent: bool) -> Result<(), String> {
+    pub async fn get(url: &str, path: &str, silent: bool) {
         let (mut output, mut downloaded) = get_output(path, silent);
 
         let res = Client::new()
@@ -15,20 +15,23 @@ impl HTTPSHandler {
             .header("Range", "bytes=".to_owned() + &downloaded.to_string() + "-")
             .send()
             .await
-            .or(Err(format!("Failed to GET from '{}'", &url)))?;
+            .or(Err(format!("Failed to GET from '{}'", &url)))
+            .unwrap();
         let total_size = downloaded
             + res
                 .content_length()
-                .ok_or(format!("Failed to get content length from '{}'", &url))?;
+                .ok_or(format!("Failed to get content length from '{}'", &url))
+                .unwrap();
 
         let pb = get_progress_bar(total_size, url, silent);
 
         let mut stream = res.bytes_stream();
         while let Some(item) = stream.next().await {
-            let chunk = item.or(Err(format!("Error while downloading.")))?;
+            let chunk = item.or(Err(format!("Error while downloading."))).unwrap();
             output
                 .write_all(&chunk)
-                .or(Err(format!("Error while writing to output.")))?;
+                .or(Err(format!("Error while writing to output.")))
+                .unwrap();
             let new = min(downloaded + (chunk.len() as u64), total_size);
             downloaded = new;
             if !silent {
@@ -41,14 +44,13 @@ impl HTTPSHandler {
                 .unwrap()
                 .finish_with_message(format!("⛵ Downloaded {} to {}", url, path));
         }
-        return Ok(());
     }
 }
 #[tokio::test]
 async fn get_works() {
     let expected_hash = "0e0f0d7139c8c7e3ff20cb243e94bc5993517d88e8be8d59129730607d5c631b";
     let out_file = "tokei-x86_64-unknown-linux-gnu.tar.gz";
-    HTTPSHandler::get("https://github.com/XAMPPRocky/tokei/releases/download/v12.0.4/tokei-x86_64-unknown-linux-gnu.tar.gz", out_file, false).await.unwrap();
+    HTTPSHandler::get("https://github.com/XAMPPRocky/tokei/releases/download/v12.0.4/tokei-x86_64-unknown-linux-gnu.tar.gz", out_file, false).await;
 
     let bytes = std::fs::read(out_file).unwrap();
     let computed_hash = sha256::digest_bytes(&bytes);
@@ -66,7 +68,7 @@ async fn get_resume_works() {
         "test/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz",
     )
     .unwrap();
-    HTTPSHandler::get("https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", out_file, false).await.unwrap();
+    HTTPSHandler::get("https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", out_file, false).await;
 
     let bytes = std::fs::read(out_file).unwrap();
     let computed_hash = sha256::digest_bytes(&bytes);
