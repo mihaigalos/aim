3e834afb709f199d50b71579c50ab30c0f650fd9
Mihai Galos <mihaigalos@gmail.com>
2021-11-15 16:51:59 +0100

    Improve naming

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ async fn main() {
     (version: crate_version!())
     (author: "Mihai Galos <mihaigalos at gmail dot com>")
     (about: "⛵ Download/upload tool.")
+    (@arg INPUT: +required +takes_value "Input to ship from.")
     (@arg OUTPUT: -o --output +takes_value "Output to ship to. If not specified, writes to stdout (cannot resume).")
     (@arg OUTPUT_ALT: -O +takes_value "Alternative to -o.")
-    (@arg INPUT: +required +takes_value "Input to ship from.")
     (@arg silent: -s --silent "Silent or quiet mode. Don't show progress meter or error messages.")
     )
     .get_matches_safe()
     .unwrap_or_else(|e| e.exit());
-    let url = args.value_of("INPUT").unwrap();
+    let input = args.value_of("INPUT").unwrap();
     let mut output = args.value_of("OUTPUT").unwrap_or("");
     if output == "" {
         output = args.value_of("OUTPUT_ALT").unwrap_or("");
@@ -21,5 +21,5 @@ async fn main() {
 
     let silent = args.is_present("silent");
 
-    ship::driver::Driver::drive(url, output, silent).await;
+    ship::driver::Driver::drive(input, output, silent).await;
 }
