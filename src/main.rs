6c45a48ada007f6f17955c4dc0fddbcaa60876f8
Mihai Galos <mihaigalos@gmail.com>
2021-11-23 20:20:26 +0100

    Remove superfluous -o,-O

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ async fn main() {
     (version: crate_version!())
     (author: "Mihai Galos <mihaigalos at gmail dot com>")
     (about: "⛵ A download/upload tool.")
-    (@arg INPUT: +required +takes_value "Input to ship from.")
-    (@arg OUTPUT: -o --output +takes_value "Explicit output to ship to. \nCan be ommitted during:\n  Downloading: if filename supplied, writes to file, otherwise stdout (cannot resume).\n  Uploading: directly uploads file to URL.")
-    (@arg OUTPUT_ALT: -O +takes_value "Alternative to -o.")
     (@arg silent: -s --silent "Silent or quiet mode. Don't show progress meter or error messages.")
+    (@arg INPUT: +required +takes_value "Input to ship from.")
+    (@arg OUTPUT: +takes_value "Explicit output to ship to. \nCan be ommitted during:\n  Downloading: if filename supplied, writes to file, otherwise stdout (cannot resume).\n  Uploading: directly uploads file to URL.")
     )
     .get_matches_safe()
     .unwrap_or_else(|e| e.exit());
