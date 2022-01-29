9703b435922fea2db0abb10e6cb4932dd90df5df
Mihai Galos <mihaigalos@gmail.com>
2022-01-29 17:26:49 +0100

    Remove superfluous

--- src/ssh.rs
+++ src/ssh.rs
@@ -30,6 +30,7 @@ impl SSHHandler {
         let parsed_address = ParsedAddress::parse_address(input, bar.silent);
         let tcp = TcpStream::connect(&ssh_host_port).unwrap();
         let mut sess = Session::new().unwrap();
+
         sess.set_tcp_stream(tcp);
         sess.handshake().expect("SSH handshake failed");
         if parsed_address.password != "" {
@@ -43,7 +44,7 @@ impl SSHHandler {
             + &parsed_address.path_segments.join("/")
             + "/"
             + &parsed_address.file);
-        println!("{}", remote_file);
+
         let (remote_file_contents, stat) = sess.scp_recv(Path::new(remote_file)).unwrap();
         let _: ssh2::Stream = remote_file_contents.stream(1);
 
@@ -54,36 +55,5 @@ impl SSHHandler {
             &mut target,
         )
         .unwrap();
-
-        //let (mut out, mut downloaded) = io::get_output(output, bar.silent);
-
-        //   let res = Client::new()
-        //       .get(input)
-        //       .header("Range", "bytes=".to_owned() + &downloaded.to_string() + "-")
-        //       .header(
-        //           reqwest::header::USER_AGENT,
-        //           reqwest::header::HeaderValue::from_static(CLIENT_ID),
-        //       )
-        //       .basic_auth(parsed_address.username, Some(parsed_address.password))
-        //       .send()
-        //       .await
-        //       .or(Err(format!("Failed to GET from {} to {}", &input, &output)))
-        //       .unwrap();
-        //   let total_size = downloaded + res.content_length().or(Some(0)).unwrap();
-        //
-        //   bar.set_length(total_size);
-        //
-        //   let mut stream = res.bytes_stream();
-        //   while let Some(item) = stream.next().await {
-        //       let chunk = item.or(Err(format!("Error while downloading."))).unwrap();
-        //       out.write_all(&chunk)
-        //           .or(Err(format!("Error while writing to output.")))
-        //           .unwrap();
-        //       let new = min(downloaded + (chunk.len() as u64), total_size);
-        //       downloaded = new;
-        //       bar.set_position(new);
-        //   }
-        //
-        //   bar.finish_download(&input, &output);
     }
 }
