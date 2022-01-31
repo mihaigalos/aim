837b95de464bb33aeb8055c06a0b8008850827b8
Mihai Galos <mihaigalos@gmail.com>
2022-01-31 21:51:48 +0100

    Use setup_session in get

--- src/ssh.rs
+++ src/ssh.rs
@@ -27,28 +27,10 @@ impl SSHHandler {
         HashChecker::check(_output, expected_sha256, bar.silent)
     }
     async fn _get(input: &str, output: &str, bar: &mut WrappedBar) {
-        let parsed_address = ParsedAddress::parse_address(input, bar.silent);
-        let tcp =
-            TcpStream::connect(&parsed_address.server).expect("Cannot connect to SSH address");
-        let mut session = Session::new().unwrap();
-
-        session.set_tcp_stream(tcp);
-        session.handshake().expect("SSH handshake failed");
-        if parsed_address.password != "" {
-            session
-                .userauth_password(&parsed_address.username, &parsed_address.password)
-                .expect("SSH Authentication failed");
-        } else {
-            session.userauth_password(&parsed_address.username, "")
-                .expect("SSH Authentication failed. No password specified. Is passwordless authentication set up?");
-        }
-        let remote_file = &(String::from("/")
-            + &parsed_address.path_segments.join("/")
-            + "/"
-            + &parsed_address.file);
+        let (session, remote_file) = SSHHandler::setup_session(input, bar.silent);
 
         let (channel, stat) = session
-            .scp_recv(Path::new(remote_file))
+            .scp_recv(Path::new(&remote_file))
             .expect(&format!("Remove file does not exist: {}", input));
 
         let mut target =
@@ -84,8 +66,8 @@ impl SSHHandler {
         Ok(())
     }
 
-    fn setup_session(output: &str, silent: bool) -> (Session, String) {
-        let parsed_address = ParsedAddress::parse_address(output, silent);
+    fn setup_session(address: &str, silent: bool) -> (Session, String) {
+        let parsed_address = ParsedAddress::parse_address(address, silent);
         let tcp =
             TcpStream::connect(&parsed_address.server).expect("Cannot connect to SSH address");
         let mut session = Session::new().unwrap();
