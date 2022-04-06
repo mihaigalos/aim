890a6374a69ea7d473884a4616632e8c94b0b001
Mihai Galos <mihaigalos@gmail.com>
2022-04-06 17:26:11 +0200

    Common.sh: Simplify

--- test/common.sh
+++ test/common.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-function err() {
-    echo -e "\e[1;31m${@}\e[0m" >&2
-    exit 1
-}
-
-function ok() {
-    echo -e "\e[1;32mOK\e[0m"
-}
-
-function highlight() {
-    echo -en "\e[1;37m${@}\e[0m"
-}
+err() { echo -e "\e[1;31m${@}\e[0m" >&2; exit 1; }
+ok() { echo -e "\e[1;32mOK\e[0m"; }
+highlight() { echo -en "\e[1;37m${@}\e[0m"; }
 
 export PATH=$PATH:$(realpath ../../target/debug/)
