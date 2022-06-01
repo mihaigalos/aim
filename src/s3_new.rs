f9c8e3f8f38eee95f87403743fbe33b7f6f629d6
Mihai Galos <mihaigalos@gmail.com>
2022-06-01 20:37:14 +0200

    S3 i-test: Just start before running

--- src/s3_new.rs
+++ src/s3_new.rs
@@ -61,34 +61,94 @@ pub fn new_s3client_with_credentials(
         region,
     ))
 }
-#[tokio::test]
-async fn test_s3_rusoto_when_typical() {
-    // let mut http_config_with_bigger_buffer = HttpConfig::new();
-    // http_config_with_bigger_buffer.read_buf_size(1024 * 1024 * 2);
-    // let http_provider = HttpClient::new_with_config(http_config_with_bigger_buffer).unwrap();
 
-    // let region = Region::Custom {
-    //     name: "us-east-1".to_owned(),
-    //     endpoint: "http://localhost:9000".to_owned(),
-    // };
-    // let credentials_provider = ChainProvider::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serial_test::serial;
 
-    // let s3 = S3Client::new_with(http_provider, credentials_provider, region);
-    let test_client = new_s3client_with_credentials(
-        Region::Custom {
-            name: "".to_owned(),
-            endpoint: "http://localhost:9000".to_owned(),
-        },
-        "minioadmin".to_owned(),
-        "minioadmin".to_owned(),
-    )
-    .unwrap();
+    fn just_start(justfile: &str) {
+        use std::env;
+        use std::io::{self, Write};
+        use std::process::Command;
+        let output = Command::new("just")
+            .args([
+                "--justfile",
+                justfile,
+                "_start",
+                env::current_dir().unwrap().to_str().unwrap(),
+            ])
+            .output()
+            .expect("failed to just _start");
 
-    // let s3 = S3Client::new(region);
-    let bucket = "test-bucket".to_string();
-    let prefix = "".to_string();
-    let obj_test = "binary_file.tar.gz.part1".to_string();
+        println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+    }
 
-    //let _objects = list_objs(s3, bucket, prefix).await;
-    let _bytes = bucket_obj_bytes(test_client, bucket, prefix, obj_test).await;
+    fn just_start_with_keys(justfile: &str) {
+        use std::env;
+        use std::io::{self, Write};
+        use std::process::Command;
+        let output = Command::new("just")
+            .args([
+                "--justfile",
+                justfile,
+                "_start_with_keys",
+                env::current_dir().unwrap().to_str().unwrap(),
+            ])
+            .output()
+            .expect("failed to just _start");
+
+        println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+    }
+
+    fn just_stop(justfile: &str) {
+        use std::env;
+        use std::process::Command;
+        let _ = Command::new("just")
+            .args([
+                "--justfile",
+                justfile,
+                "_stop",
+                env::current_dir().unwrap().to_str().unwrap(),
+            ])
+            .output();
+    }
+    #[tokio::test]
+    #[serial]
+    async fn test_s3_rusoto_when_typical() {
+        // let mut http_config_with_bigger_buffer = HttpConfig::new();
+        // http_config_with_bigger_buffer.read_buf_size(1024 * 1024 * 2);
+        // let http_provider = HttpClient::new_with_config(http_config_with_bigger_buffer).unwrap();
+
+        // let region = Region::Custom {
+        //     name: "us-east-1".to_owned(),
+        //     endpoint: "http://localhost:9000".to_owned(),
+        // };
+        // let credentials_provider = ChainProvider::new();
+
+        // let s3 = S3Client::new_with(http_provider, credentials_provider, region);
+        just_start("test/s3/Justfile");
+        let test_client = new_s3client_with_credentials(
+            Region::Custom {
+                name: "".to_owned(),
+                endpoint: "http://localhost:9000".to_owned(),
+            },
+            "minioadmin".to_owned(),
+            "minioadmin".to_owned(),
+        )
+        .unwrap();
+
+        // let s3 = S3Client::new(region);
+        let bucket = "test-bucket".to_string();
+        let prefix = "".to_string();
+        let obj_test = "binary_file.tar.gz.part1".to_string();
+
+        //let _objects = list_objs(s3, bucket, prefix).await;
+        let _bytes = bucket_obj_bytes(test_client, bucket, prefix, obj_test).await;
+        just_stop("test/s3/Justfile");
+    }
 }
