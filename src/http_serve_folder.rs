49f72a4dee83a02c38c0d0b5b10d2f93cf6b4316
Mihai Galos <mihaigalos@gmail.com>
2022-06-12 12:39:00 +0200

    Http serve folder: Simplify errors

--- src/http_serve_folder.rs
+++ src/http_serve_folder.rs
@@ -1,16 +1,13 @@
-use crate::error::ValidateError;
-use warpy;
+use std::io;
+
+extern crate warpy;
 
 pub struct WarpyWrapper;
 
 impl WarpyWrapper {
-    pub async fn run(folder: String) -> Result<(), ValidateError> {
+    pub async fn run(folder: String) -> io::Result<()> {
         let ip = [0, 0, 0, 0];
         let footer = format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-
-        warpy::server::run(folder, ip, footer, None, false)
-            .await
-            .unwrap();
-        Ok(())
+        Ok(warpy::server::run(folder, ip, footer, None, false).await?)
     }
 }
