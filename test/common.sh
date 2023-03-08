c50c8d52549da565922290c986927dc0f078f201
Mihai Galos <mihaigalos@gmail.com>
2023-03-08 08:00:42 +0100

    Wait for 5s for Dockers to start

--- test/common.sh
+++ test/common.sh
@@ -5,8 +5,9 @@ ok() { echo -e "\e[1;32mOK\e[0m"; }
 highlight() { echo -en "\e[1;37m${@}\e[0m"; }
 function wait_docker() {
     while [ $(docker ps | grep $1 | wc -l) -eq 0 ]; do
-      sleep 0.5
-    done 
+      sleep 5
+    done
+    sleep 5
 }
 
 
